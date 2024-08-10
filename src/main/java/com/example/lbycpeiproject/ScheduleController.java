cfdb60bb5ed3489cedcdf4c0a8e2987b29aebc1a
VenzGerolaga <156872298+VenzGerolaga@users.noreply.github.com>
2024-08-10 15:04:25 +0800

    Add files via upload

--- src/main/java/com/example/lbycpeiproject/ScheduleController.java
+++ src/main/java/com/example/lbycpeiproject/ScheduleController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -15,20 +16,27 @@ import javafx.event.ActionEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import javafx.scene.layout.AnchorPane;
 
 public class ScheduleController {
 
     @FXML
-    private StackPane schedulePane;
+    private AnchorPane schedulePane;
+
+    @FXML
+    private TextField courseDay;
+
+    @FXML
+    private TextField courseName;
+
+    @FXML
+    private TextField courseTime;
 
     private List<ScheduledClass> scheduledClasses;
 
     @FXML
     public void initialize() {
-        // Assuming the scheduled classes are loaded from another FXML file
-        List<ScheduledClass> classes = new ArrayList<>();
-        scheduledClasses = classes;
-        createSchedule();
+        scheduledClasses = new ArrayList<>();
     }
 
     private void createSchedule() {
@@ -38,60 +46,65 @@ public class ScheduleController {
     }
 
     private void createClassNode(ScheduledClass scheduledClass) {
-        StackPane classNode = new StackPane();
-        Rectangle rectangle = new Rectangle(132, 24 * scheduledClass.getDuration());
-        rectangle.setFill(Color.web("#87B758"));
-        Text label = new Text(scheduledClass.getName() + " " + scheduledClass.getStartTime() + " - " + scheduledClass.getEndTime());
-        label.setFont(Font.font("Comic Sans", 12));
-        classNode.getChildren().addAll(rectangle, label);
+        for (int day : scheduledClass.getDays()) {
+            int x = getXPosition(day);
+            double y = getYPosition(scheduledClass.getStartTime());
 
-        int x = getXPosition(scheduledClass.getDay());
-        int y = getYPosition(scheduledClass.getStartTime());
-        classNode.setLayoutX(x);
-        classNode.setLayoutY(y);
+            StackPane classNode = new StackPane();
+            Rectangle rectangle = new Rectangle();
+            rectangle.setWidth(132);
+            rectangle.setHeight(24 * scheduledClass.getDuration());
+            rectangle.setFill(Color.web("#87B758"));
+            Text label = new Text(scheduledClass.getName() + " " + scheduledClass.getStartTime() + " - " + scheduledClass.getEndTime());
+            label.setFont(Font.font("Comic Sans", 12));
+            label.setFill(Color.WHITE);
+            classNode.getChildren().addAll(rectangle, label);
 
-        schedulePane.getChildren().add(classNode);
+            classNode.setLayoutX(x);
+            classNode.setLayoutY(y);
+            schedulePane.getChildren().add(classNode);
+        }
     }
 
     private int getXPosition(int day) {
-        // Use the provided table to calculate the x position based on the day
         switch (day) {
             case 0: // Monday
-                return 180;
+                return 111;
             case 1: // Tuesday
-                return 312;
+                return 243;
             case 2: // Wednesday
-                return 444;
+                return 375;
             case 3: // Thursday
-                return 576;
+                return 507;
             case 4: // Friday
-                return 708;
+                return 639;
             case 5: // Saturday
-                return 840;
+                return 771;
             default:
                 return 0;
         }
     }
 
-    private int getYPosition(String startTime) {
+    private double getYPosition(String startTime) {
         String[] parts = startTime.split(":");
         int hours = Integer.parseInt(parts[0]);
         int minutes = Integer.parseInt(parts[1]);
 
-        int totalMinutes = hours * 60 + minutes;
-        return 220 + ((totalMinutes - 450) / 30) * 24;
+        double totalMinutes = hours * 60 + minutes;
+        double startMinutes = ((totalMinutes - 450) / 60.0) * 24;
+        return 220 + startMinutes;
     }
 
-    private static class ScheduledClass {
-        private final String name;
-        private final int day;
-        private final String startTime;
-        private final String endTime;
-        private final double duration;
+    public static class ScheduledClass {
+        private String name;
+        private int[] days;
+        private String startTime;
+        private String endTime;
+        private double duration;
 
-        public ScheduledClass(String name, String day, String startTime, String endTime, double duration) {
+        public ScheduledClass(String name, String days, String startTime, String endTime, double duration) {
             this.name = name;
-            this.day = convertDayToIndex(day);
+            this.days = convertDaysToIndices(days);
             this.startTime = startTime;
             this.endTime = endTime;
             this.duration = duration;
@@ -101,35 +114,67 @@ public class ScheduleController {
             return name;
         }
 
-        public int getDay() {
-            return day;
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int[] getDays() {
+            return days;
+        }
+
+        public void setDays(int[] days) {
+            this.days = days;
         }
 
         public String getStartTime() {
             return startTime;
         }
 
+        public void setStartTime(String startTime) {
+            this.startTime = startTime;
+        }
+
         public String getEndTime() {
             return endTime;
+        }
+
+        public void setEndTime(String endTime) {
+            this.endTime = endTime;
         }
 
         public double getDuration() {
             return duration;
         }
 
-        private int convertDayToIndex(String day) {
+        public void setDuration(double duration) {
+            this.duration = duration;
+        }
+
+        private static int[] convertDaysToIndices(String daysString) {
+            String[] daysArray = daysString.replaceAll("\\s+", "").split(",");
+            List<Integer> dayIndices = new ArrayList<>();
+            for (String day : daysArray) {
+                int index = convertDayToIndex(day);
+                if (index != -1) {
+                    dayIndices.add(index);
+                }
+            }
+            return dayIndices.stream().mapToInt(Integer::intValue).toArray();
+        }
+
+        private static int convertDayToIndex(String day) {
             switch (day) {
-                case "Monday":
+                case "M":
                     return 0;
-                case "Tuesday":
+                case "T":
                     return 1;
-                case "Wednesday":
+                case "W":
                     return 2;
-                case "Thursday":
+                case "TH":
                     return 3;
-                case "Friday":
+                case "F":
                     return 4;
-                case "Saturday":
+                case "S":
                     return 5;
                 default:
                     return -1;
@@ -138,7 +183,78 @@ public class ScheduleController {
     }
 
     @FXML
-    void returnWindow(ActionEvent event) {
+    private void addClasses(ActionEvent event) {
+        String name = courseName.getText();
+        String dayString = courseDay.getText();
+        String timeString = courseTime.getText();
+        String[] timeParts = timeString.split("-");
+        String startTime = timeParts[0];
+        String endTime = timeParts[1];
+        ScheduledClass newClass = new ScheduledClass(name, dayString, startTime, endTime, getClassDuration(startTime, endTime));
+        scheduledClasses.add(newClass);
+        createSchedule();
+        clearTextFields();
+    }
+
+    @FXML
+    void removeClasses(ActionEvent event) {
+        String name = courseName.getText();
+        for (int i = 0; i < schedulePane.getChildren().size(); i++) {
+            Node node = schedulePane.getChildren().get(i);
+            if (node instanceof StackPane) {
+                StackPane classNode = (StackPane) node;
+                Text label = (Text) classNode.getChildren().get(1);
+                String className = label.getText().split(" ")[0];
+                if (className.equals(name)) {
+                    schedulePane.getChildren().remove(i);
+                    i--;
+                }
+            }
+        }
+
+        scheduledClasses.removeIf(scheduledClass -> scheduledClass.getName().equals(name));
+        clearTextFields();
+    }
+
+    @FXML
+    private void editClasses(ActionEvent event) {
+        String name = courseName.getText();
+        String dayString = courseDay.getText();
+        String timeString = courseTime.getText();
+        String[] timeParts = timeString.split("-");
+        String startTime = timeParts[0];
+        String endTime = timeParts[1];
+
+        for (int i = 0; i < schedulePane.getChildren().size(); i++) {
+            Node node = schedulePane.getChildren().get(i);
+            if (node instanceof StackPane) {
+                StackPane classNode = (StackPane) node;
+                Text label = (Text) classNode.getChildren().get(1);
+                String className = label.getText().split(" ")[0];
+                if (className.equals(name)) {
+                    schedulePane.getChildren().remove(i);
+                    i--;
+                }
+            }
+        }
+
+        for (int i = 0; i < scheduledClasses.size(); i++) {
+            ScheduledClass scheduledClass = scheduledClasses.get(i);
+            if (scheduledClass.getName().equals(name)) {
+                scheduledClass.setName(name);
+                scheduledClass.setDays(ScheduledClass.convertDaysToIndices(dayString));
+                scheduledClass.setStartTime(startTime);
+                scheduledClass.setEndTime(endTime);
+                scheduledClass.setDuration(getClassDuration(startTime, endTime));
+                createSchedule();
+                break;
+            }
+        }
+        clearTextFields();
+    }
+
+    @FXML
+    protected void returnWindow(ActionEvent event) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-screen.fxml"));
             Parent root = fxmlLoader.load();
@@ -149,5 +265,27 @@ public class ScheduleController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void clearTextFields() {
+        courseName.clear();
+        courseDay.clear();
+        courseTime.clear();
+    }
+
+    private double getClassDuration(String startTime, String endTime) {
+        String[] startTimeParts = startTime.split(":");
+        int startHours = Integer.parseInt(startTimeParts[0]);
+        int startMinutes = Integer.parseInt(startTimeParts[1]);
+
+        String[] endTimeParts = endTime.split(":");
+        int endHours = Integer.parseInt(endTimeParts[0]);
+        int endMinutes = Integer.parseInt(endTimeParts[1]);
+
+        int totalStartMinutes = startHours * 60 + startMinutes;
+        int totalEndMinutes = endHours * 60 + endMinutes;
+        double totalDurationMinutes = (totalEndMinutes - totalStartMinutes) / 60.0;
+
+        return totalDurationMinutes;
     }
 }
